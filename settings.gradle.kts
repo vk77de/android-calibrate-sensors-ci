210b7edf4744656403ee2b0640f12919fb72956f
Victor Klimov <vk77de@googlemail.com>
2026-06-27 10:38:47 +0200

    new settings.gradle.kts file

---
+++ settings.gradle.kts
@@ -0,0 +1,16 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+rootProject.name = "Sensors calibration Kotlin Android"
+include(":app")
