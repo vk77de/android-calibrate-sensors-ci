cecaacc5eb8107033a7adf9b5c6a27ab48d424e6
Victor Klimov <vk77de@googlemail.com>
2026-06-27 13:48:58 +0200

    modified for ktlint

--- app/src/main/java/com/example/helloworldkotlinandroid/MainActivity.kt
+++ app/src/main/java/com/example/helloworldkotlinandroid/MainActivity.kt
@@ -9,13 +9,13 @@ import android.location.Location
 import android.location.LocationListener
 import android.location.LocationManager
 import android.os.Bundle
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.camera.core.CameraSelector
 import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.camera.view.PreviewView
-import android.widget.TextView
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import java.util.concurrent.ExecutorService
